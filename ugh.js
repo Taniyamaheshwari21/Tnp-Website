e99a97ef0ae98b39ca53fa267a684df06564bf9d
Taniyamaheshwari21 <133154389+Taniyamaheshwari21@users.noreply.github.com>
2024-09-04 19:58:32 +0530

    Create ugh.js

---
+++ ugh.js
@@ -0,0 +1,10 @@
+//var count=0;
+
+//count++;
+function clickEvent() {  
+    //if(count==1){
+        window.alert("You have successfully applied to this company.More details will be shared to you via SMS or mail to you.")
+        const get=document.getElementById('btn');
+        get.click();
+        
+    }
